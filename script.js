61cf49fdae78e3e20cf1502bb3766ab30baed533
leandro <leandrojustino531@gmail.com>
2026-04-12 00:53:44 -0300

    Refactor item management and add event handlers
    Refactored item addition and removal logic, added click event handlers for alert and background changes.

--- script.js
+++ script.js
@@ -1,15 +1,41 @@
-document.getElementById("btnAdicionar").onclick = function() {
-    let lista = document.getElementById("lista");
-    let li = document.createElement("li");
-    li.innerText = "novo item";
-    lista.appendChild(li);}
+let count = 0;
 
+function add() {
+  let input = document.getElementById('i');
+  let texto = input.value.trim();
+  if (texto !== "") {
+    count++;
+    document.getElementById('contador').textContent = count;
+    
+    let li = document.createElement('li');
+    li.innerHTML = texto + '<button onclick="remover(this.parentElement)">x</button>';
+    
+    let lista = document.getElementById('lista');
+    lista.appendChild(li);
+    
+    input.value = '';
+    input.focus();
+  }
+}
 
-
-
-let contador = 0;
-
-document.getElementById("btnClique").addEventListener("click", function() {
-  contador++;
-  console.log("botão clicado " + contador + " vezes");
+function remover(li) {
+  li.remove();
+  count--;
+  document.getElementById('contador').textContent = count;
+}
+ 
+ /* alerta */
+let clique = document.getElementById("i");
+clique.addEventListener("click", function () {
+    alert("!!!");
 });
+
+/* fundo vira ouro */
+document.querySelector("body").onclick= function() {
+  this.style.backgroundImage = "url('https://media1.tenor.com/m/B8RqZAbFJUwAAAAd/minecraft-calm.gif')";
+
+}
+/* fundo do input muda*/
+document.querySelector("input").onclick= function() {
+  this.style.backgroundImage = "url('https://previews.123rf.com/images/corbendallas/corbendallas1811/corbendallas181100577/127490370-light-pink-square-mosaic-tiles-background-background-with-abstract-polygonal-pixels-random-tiles.jpg')";
+}
